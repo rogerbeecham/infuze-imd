e0fade19001e55d651abb52a7a8de0395eebf20e
Roger Beecham <r.j.beecham@leeds.ac.uk>
2025-11-10 11:36:21 +0000

    spacing edits (mainly)

--- code/theme_infuze.R
+++ code/theme_infuze.R
@@ -8,24 +8,24 @@ update_geom_defaults("text", list(family = "Arial"))
 update_geom_defaults("richtext", list(family = "Arial"))
 
 
-theme_infuze_light <- function(base_size = 14) {
+theme_infuze_light <- function(base_size = 12) {
   return <- theme_classic(base_size, base_family="Arial") +
-    theme(plot.title = element_markdown(size = rel(1.4), colour="#141432"),
-          plot.subtitle = element_markdown(size = rel(1.0), colour="#5a2337"),
-          plot.caption = element_markdown(size = rel(.8), colour="#3c503c",
-                                      margin = margin(t = 10)),
-          plot.tag = element_markdown(size = rel(.9), colour="#3c503c"),
-          strip.text = element_markdown(size = rel(.9)),
-          strip.text.x = element_markdown(margin = margin(t = 1, b = 1)),
+    theme(
+      plot.title = element_markdown(size = 16, colour="#141432", face="bold"),
+      plot.subtitle = element_markdown(size = 16, colour="#5a2337", margin=margin(b=40)),
+          plot.caption = element_markdown(size = 12, colour="#5a2337", margin=margin(t=35)),
+          plot.tag = element_markdown(size = 12, colour="#3c503c"),
+          strip.text = element_markdown(size = 12, colour="#3c503c"),
+          #strip.text.x = element_markdown(size = 14, margin = margin(t = 1, b = 1), colour="#3c503c"),
           strip.background = element_rect(fill = "#ebedeb",  linewidth=0),
           panel.border = element_blank(),
           plot.background = element_rect(fill = "#ebedeb",  linewidth=0),
           panel.background = element_rect(fill = "#ebedeb",  linewidth=0),
           axis.ticks = element_blank(),
-          axis.title.x = element_markdown(margin = margin(t = 10)),
-          axis.title.y = element_markdown(margin = margin(r = 10)),
+          axis.title.x = element_markdown(size = 14, margin = margin(t = 10), face="bold", colour="#141432"),
+          axis.title.y = element_markdown(size = 14, margin = margin(r = 10), face="bold", colour="#141432"),
           axis.line = element_line(linewidth = .2),
-          legend.title = element_markdown(size = rel(0.8)),
+          legend.title = element_markdown(size = 12, colour="#3c503c"),
           legend.background = element_rect(fill = "#ebedeb",  linewidth=0),
           legend.position = "bottom")
   
